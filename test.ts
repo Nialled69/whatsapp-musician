11f70dfc01130e77effe2123d4c0e355424abeca
Nialled69 <dj.nialled.007.1d@gmail.com>
2024-05-10 19:31:55 +0530

    Testing some logics

--- test.ts
+++ test.ts
@@ -1,9 +1,11 @@
-// This is just a Test file to test modules. Imagine opening this L-BOZO    XD
+// This is just a Test file to test modules. This doesn't contribute anything to the original project. Imagine opening this L-BOZO    XD
 
 import fs from 'fs';
 import path from 'path';
 import ytdl from 'ytdl-core';
 import yts from 'yt-search';
+import {spawn} from 'child_process';
+import ffmpeg from 'fluent-ffmpeg';
 
 if(!fs.existsSync('./testing')){
     fs.mkdirSync('testing');
@@ -26,3 +28,53 @@ files.forEach((file) => {
 });
 
 console.log(cache);
+
+/*try {
+    const vids = await yts("Yoasabi Idol");
+    const {videoId , url} = vids.videos[0];
+    const downloadPath = `downloads/${videoId}.mp3`;
+
+    const moosic = ytdl(url); 
+    moosic.on("error", error => {
+        console.error("Error downloading audio:", error);
+    });
+
+    const ffmpegPath = 'D:/ffmpeg/bin/ffmpeg.exe'; 
+
+    const ffmpegArgs = [
+        '-f', 'opus', 
+        '-i', '-', 
+        '-codec:a', 'libmp3lame',
+        '-q:a', '1',
+        '-y', 
+        downloadPath 
+    ];
+
+    const ffmpegProcess = spawn(ffmpegPath, ffmpegArgs);
+
+    const outputStream = fs.createWriteStream(downloadPath);
+    ffmpegProcess.stderr.pipe(process.stderr); 
+    ffmpegProcess.stdout.pipe(outputStream);
+
+    moosic.pipe(ffmpegProcess.stdin);
+
+    ffmpegProcess.on('close', (code) => {
+        if (code === 0) {
+            console.log(`Audio file saved successfully: ${downloadPath}`);
+        } else {
+        }
+    });
+
+    ffmpegProcess.on('error', (error) => {
+        console.error('ffmpeg process error:', error);
+    });
+
+    ffmpegProcess.on('exit', (code) => {
+        if (code !== 0) {
+            console.error(`ffmpeg process exited with code ${code}`);
+        }
+    });
+
+} catch (error) {
+    console.error('Error:', error);
+}*/
